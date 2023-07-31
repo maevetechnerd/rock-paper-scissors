938cd79ff95470866f2b38d7ce95125c2ca617f5
maevetechnerd <maefin@tutanota.com>
2023-07-31 11:34:14 +0200

    Managed to make 1 round game of rock paper scissors. Now working on getting it to be case insensitive so I can make it a 5 round game

--- script/rps.js
+++ script/rps.js
@@ -1,16 +1,21 @@
-function getComputerChoice() {
+function getComputerChoice(rpsAi) {
     const rps = ["Rock", "Paper", "Scissor"];
-    const rpsAi = rps[Math.floor(Math.random() * rps.length)];
-    return rps;
+    rpsAi = rps[Math.floor(Math.random() * rps.length)];
+    return rpsAi;
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection < computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissor" || playerSelection === "Scissor" && computerSelection === "Rock") {
         return `${computerSelection} beats ${playerSelection}, you lose!`;
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Scissor" || playerSelection === "Scissor" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Rock") {
+        return `${playerSelection} beats ${computerSelection}, you win!`;
+    }
+    else if (playerSelection === computerSelection) {
+        return `${playerSelection} and ${computerSelection} are the same. It is a draw!`;
     }
 }
 
-const playerSelection = "rock";
-playerSelection.toUpperCase();
+const playerSelection = prompt("Rock, Paper or Scissor?", "");
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
